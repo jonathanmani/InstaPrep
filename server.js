62907c9100a9d83b2a2c99f5de82c067bc6f89e3
Jonathan Mani <jonathanmani@protonmail.com>
2022-09-02 11:40:06 -0400

    solved issues with connecting ot MongoDB

--- server.js
+++ server.js
@@ -3,17 +3,17 @@ const app = express()
 const mongoose = require('mongoose')
 const passport = require('passport')
 const session = require('express-session')
-const MongoStore = require('connect-mongo')(session)
+const MongoStore = require('connect-mongo')
 const flash = require('express-flash')
 const logger = require('morgan')
 const connectDB = require('./config/database')
-const mainRoutes = require('./routes/main')
-const todoRoutes = require('./routes/todos')
+// const mainRoutes = require('./routes/main')
+// const todoRoutes = require('./routes/todos')
 
 require('dotenv').config({path: './config/.env'})
 
 // Passport config
-require('./config/passport')(passport)
+// require('./config/passport')(passport)
 
 connectDB()
 
@@ -28,7 +28,10 @@ app.use(
       secret: 'keyboard cat',
       resave: false,
       saveUninitialized: false,
-      store: new MongoStore({ mongooseConnection: mongoose.connection }),
+      store: MongoStore.create({
+        mongoUrl:process.env.DB_STRING,
+        mongooseConnection: mongoose.connection 
+    }),
     })
   )
   
@@ -38,8 +41,8 @@ app.use(passport.session())
 
 app.use(flash())
   
-app.use('/', mainRoutes)
-app.use('/todos', todoRoutes)
+// app.use('/', mainRoutes)
+// app.use('/todos', todoRoutes)
  
 app.listen(process.env.PORT, ()=>{
     console.log(`Connection Successful: Server running on port ${process.env.PORT}`)
