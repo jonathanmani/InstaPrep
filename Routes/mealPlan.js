de51e1887a7a30b7bd0a592a937ecf8b643f767b
Jonathan Mani <39526978+jonathanmani@users.noreply.github.com>
2022-09-26 10:36:21 -0400

    added the Profile Schema

--- Routes/mealPlan.js
+++ Routes/mealPlan.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
-//const mealPlanController = require("../controllers/mealPlan");
+const mealPlanController = require("../controllers/mealPlan");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
-// router.get("/:id", ensureAuth, mealPlanController.getPost);
+router.get("/:id", ensureAuth, mealPlanController.getPost);
 
-// router.post("/createPost", upload.single("file"), postsController.createPost);
+router.post("/createPost", upload.single("file"), postsController.createPost);
 
-// router.put("/likePost/:id", postsController.likePost);
+router.put("/likePost/:id", postsController.likePost);
 
-// router.delete("/deletePost/:id", postsController.deletePost);
+router.delete("/deletePost/:id", postsController.deletePost);
 
 module.exports = router;
