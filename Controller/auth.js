3db927d46a46779c76b094322a525d9ef0972b26
Jonathan Mani <39526978+jonathanmani@users.noreply.github.com>
2022-09-27 17:54:21 -0400

    added tailwind

--- Controller/auth.js
+++ Controller/auth.js
@@ -40,12 +40,13 @@ exports.logout = (req, res) => {
   req.logout(() => {
     console.log('User has logged out.')
   })
-    req.session.destroy((err) => {
-      if (err) console.log('Error : Failed to destroy the session during logout.', err)
-      req.user = null
-      res.redirect('/')
-    })
-}
+  req.session.destroy((err) => {
+    if (err)
+      console.log("Error : Failed to destroy the session during logout.", err);
+    req.user = null;
+    res.redirect("/");
+  });
+};
   
 exports.getSignup = (req, res) => {
     if (req.user) {
