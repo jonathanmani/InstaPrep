41c84d2def9e7f0f8bfcaf9158e0182fb1d1867e
Jonathan Mani <39526978+jonathanmani@users.noreply.github.com>
2022-10-10 11:22:19 -0400

    resolved a bug where mealPlan arrays were not being put in the mealplan schema

--- Controller/mealPlan.js
+++ Controller/mealPlan.js
@@ -14,34 +14,30 @@ module.exports ={
     },
     addMealPlan: async(req, res) => {
         try {
-            const recipe = await Recipe.find();
+            let recipe = await Recipe.find();
             let mealPlanLunch = []
             let mealPlanDinner = []
+            let mealtype = req.body.mealType
             let lunches = recipe
-                .filter(recipe => {
-                    recipe.type.includes('Lunch')
-                })
+                .filter(recipe => recipe.type.includes('Lunch'))
                 .sort(()=>Math.random()-0.5)
             console.log('lunches',lunches)
-            //let shuffledLunches = lunches.sort(()=>Math.random()-0.5)
             let dinners = recipe
-                .filter(recipe => {
-                    recipe.type.includes('Dinner')
-                })
+                .filter(recipe => recipe.type.includes('Dinner'))
                 .sort(()=>Math.random()-0.5)
             console.log('dinners',dinners)
-            //let shuffledDinners = dinners.sort(()=>Math.random()-0.5)
-            if (req.body.mealType.includes('Lunch') && req.body.mealType.includes('Dinner') ){
-                for (let i = 0; i < req.body.daysCooking.length; i++){
+
+            if (mealtype.includes('Lunch') && mealtype.includes('Dinner') ){
+                for (let i = 0; i < (req.body.daysCooking).length; i++){
                     mealPlanLunch.push(lunches[i])
                     mealPlanDinner.push(dinners[i])
                 }
-            } else if (req.body.mealType.includes('Lunch')){
-                for (let i = 0; i < req.body.daysCooking.length; i++){
+            } else if (mealtype.includes('Lunch')){
+                for (let i = 0; i < (req.body.daysCooking).length; i++){
                     mealPlanLunch.push(lunches[i])
                 }
-            } else {
-                for (let i = 0; i < req.body.daysCooking.length; i++){
+            } else if (mealtype.includes('Dinner')) {
+                for (let i = 0; i < (req.body.daysCooking).length; i++){
                     mealPlanDinner.push(dinners[i])
                 }
             }
@@ -55,8 +51,8 @@ module.exports ={
                 mealType: req.body.mealType,
                 favorite: req.body.favorite,
                 user: req.user.id,
-                lunch: mealPlanLunch,
-                dinner: mealPlanDinner
+                lunches: mealPlanLunch,
+                dinners: mealPlanDinner
             });
             console.log('added Meal Plan')
             res.redirect('/profile')
@@ -69,22 +65,12 @@ module.exports ={
         try {
             const mealPlan = await MealPlan.findById(req.params.id)
             const recipe = await Recipe.find();
-            let lunches = recipe.filter(recipe => {
-                recipe.type.includes('Lunch')
-            })
-            let shuffledLunches = lunches.sort(()=>Math.random()-0.5)
-            let dinners = recipe.filter(recipe => {
-                recipe.type.includes('Dinner')
-            })
-            let shuffledDinners = dinners.sort(()=>Math.random()-0.5)
             
             const user = await User.findOne({ _id: req.user.id })
             console.log(user);
             res.render('mealPlan.ejs', { 
                 mealPlan: mealPlan, 
-                user: user, 
-                lunches: shuffledLunches,
-                dinners: shuffledDinners
+                user: user
             });
         } catch (err) {
             console.log(err)
