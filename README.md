3a893a559eaefa6150b28aed4e7cf65a22fb20f7
Jonathan Mani <39526978+jonathanmani@users.noreply.github.com>
2022-10-17 11:27:00 -0400

    major update to README

--- README.md
+++ README.md
@@ -1,4 +1,39 @@
-# MealPlanner
+# IntaPrep
 
-integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
-      crossorigin="anonymous"
+A mealprep app that minimizes the time you spend planning out your meals for the week. Load recipes in your cookbook, and with the click of a button, InstaPrep will list the meals you'll be eating for any specific day
+
+![InstaPrep](InstaPrep.png "InstaPrep")
+
+## How It's Made
+
+**Tech:** bcrypt, connect-mongo, dotenv, ejs, express, express-flash, express-session, mongodb, mongoose, morgan, nodemon, passport, passport-local, validator
+
+Recipes are added to the database using a form that handles a POST request. These recipes are created on the form, sent to the MongoDB database and retrieved and displayed to the user on their profile using EJS. Recipes can be favorited using an update method.
+
+Meal plans are created using a form in which the user selects the type of meal, the days they will be cooking and start/end date of the meal plan. From this data, recipes are randomized and displayed to the user
+
+Users are authenticated using PassportJS so anyone can have their own account. Passwords are hashed using bcrypt to protect user privacy.
+
+## Optimizations
+
+Core functionality on the app is completed. As per requests from friends and family, I will be implementing these features in the future:
+
+- Adding greater personalization to the meals such as: type of cuisine, nutritional information
+- ability to add meals by copying and pasting a URL
+- formatting the HTML so that printing a page will render data with a clean UI
+
+## Lessons Learned
+
+While building this app, I've learned that it's important to keep track of IDs that exist between the various models of the app. If proper links are not established with the methods, the server will not return anything.
+
+Additionally, EJS may throw errors if data contains undefined elements. It's best to render entire objects within EJS and parse through properties as necessary.
+
+I've also learned how to use moment to display dates correctly as well as retrieve certain days between 2 parameters.
+
+## Check Out My Other Work
+
+[Pomodoro Task Tracker](https://github.com/jonathanmani/Pomodoro-Task-Tracker) - Create tasks, set your priority and record pomodoro sessions with this all-in-one task tracker
+
+[Pomodoro Task Tracker](https://github.com/jonathanmani/DailyUI) - A collection of Front End designs made using TailwindCss and React
+
+[CodeWars](https://github.com/jonathanmani/CodeWars) - A collection of all the CodeWars problems I've done to practice my knowledge in Javascript.
--- server.js
+++ server.js
@@ -1,57 +1,58 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const passport = require('passport')
-const session = require('express-session')
-const MongoStore = require('connect-mongo')
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
 const methodOverride = require("method-override");
-const flash = require('express-flash')
-const logger = require('morgan')
-const connectDB = require('./config/database')
-const mainRoutes = require('./routes/main')
-const mealPlanRoutes = require('./routes/mealPlan')
-const recipeRoutes = require('./routes/recipe')
-const path = require('path')
-require('dotenv').config({path: './config/.env'})
+const flash = require("express-flash");
+const logger = require("morgan");
+const connectDB = require("./config/database");
+const mainRoutes = require("./routes/main");
+const mealPlanRoutes = require("./routes/mealPlan");
+const recipeRoutes = require("./routes/recipe");
+const path = require("path");
+require("dotenv").config({ path: "./config/.env" });
 
 //Passport config
-require('./config/passport')(passport) 
+require("./config/passport")(passport);
 
-connectDB()
+connectDB();
 
-
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(logger('dev'))
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(logger("dev"));
 
 // Use forms for PUT / delete
-app.use(methodOverride('_method'))
+app.use(methodOverride("_method"));
 
 // Sessions
 app.use(
-    session({
-      secret: 'keyboard cat',
-      resave: false,
-      saveUninitialized: false,
-      store: MongoStore.create({
-        mongoUrl:process.env.DB_STRING,
-        mongooseConnection: mongoose.connection 
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.DB_STRING,
+      mongooseConnection: mongoose.connection,
     }),
-    })
-  )
-  
-// Passport middleware
-app.use(passport.initialize())
-app.use(passport.session())
+  })
+);
 
-app.use(flash())
-  
-app.use('/', mainRoutes)
-app.use('/recipe',recipeRoutes)
-app.use('/mealPlan', mealPlanRoutes)
- 
-app.listen(process.env.PORT, ()=>{
-    console.log(`Connection Successful: Server running on port ${process.env.PORT}`)
-})    
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
+
+app.use("/", mainRoutes);
+app.use("/recipe", recipeRoutes);
+app.use("/mealPlan", mealPlanRoutes);
+
+app.listen(process.env.PORT, () => {
+  console.log(
+    `Connection Successful: Server running on port ${process.env.PORT}`
+  );
+});
