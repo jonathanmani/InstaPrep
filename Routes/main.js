b6cd58ccf6e87a25ebe8cf022bcb46545d071e81
Jonathan Mani <39526978+jonathanmani@users.noreply.github.com>
2022-10-20 12:24:50 -0400

    changed folder names

--- Routes/main.js
+++ Routes/main.js
@@ -1,17 +1,17 @@
-const express = require('express')
-const router = express.Router()
-const authController = require('../controller/auth')
-const homeController = require('../controller/home')
-const { ensureAuth, ensureGuest } = require('../middleware/auth')
+const express = require("express");
+const router = express.Router();
+const authController = require("../Controller/auth");
+const homeController = require("../Controller/home");
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
-router.get('/', homeController.getIndex)
-router.get('/profile', ensureAuth, homeController.getProfile);
+router.get("/", homeController.getIndex);
+router.get("/profile", ensureAuth, homeController.getProfile);
 router.get("/favorites", homeController.getFavorites);
 router.get("/allMealPlans", homeController.getMealPlan);
-router.get('/login', authController.getLogin)
-router.get('/logout', authController.logout)
-router.get('/signup', authController.getSignup)
-router.post('/login', authController.postLogin)
-router.post('/signup', authController.postSignup)
+router.get("/login", authController.getLogin);
+router.get("/logout", authController.logout);
+router.get("/signup", authController.getSignup);
+router.post("/login", authController.postLogin);
+router.post("/signup", authController.postSignup);
 
-module.exports = router
+module.exports = router;
--- routes/recipe.js
+++ routes/recipe.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const upload = require("../middleware/multer");
-const recipeController = require("../controller/recipe");
+const recipeController = require("../Controller/recipe");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 router.get("/:id", ensureAuth, recipeController.getRecipe);
 router.post("/addRecipe", upload.single("file"), recipeController.addRecipe);
-router.post("/createRecipe", upload.single("file"), recipeController.createRecipe);
+router.post(
+  "/createRecipe",
+  upload.single("file"),
+  recipeController.createRecipe
+);
 router.put("/favoriteRecipe/:id", recipeController.favoriteRecipe);
 router.put("/unfavoriteRecipe/:id", recipeController.unfavoriteRecipe);
-router.put("/editRecipe/:id", recipeController.editRecipe)
-router.put("/updateRecipe/:id", recipeController.updateRecipe)
+router.put("/editRecipe/:id", recipeController.editRecipe);
+router.put("/updateRecipe/:id", recipeController.updateRecipe);
 router.delete("/deleteRecipe/:id", recipeController.deleteRecipe);
 
 module.exports = router;
